5de5524aedbccb3442abc7d71f2b887737057492
romejg <justineguillermo00@gmail.com>
2024-06-17 21:09:23 +0800

    removed target in hello cargo

--- 1.getting-started/2-hello-cargo/target/debug/deps/hello_cargo-404aecc627eac8d2.d
+++
@@ -1,5 +0,0 @@
-/home/rome/Documents/prog/rust/rust-intro/2-hello-cargo/target/debug/deps/libhello_cargo-404aecc627eac8d2.rmeta: src/main.rs
-
-/home/rome/Documents/prog/rust/rust-intro/2-hello-cargo/target/debug/deps/hello_cargo-404aecc627eac8d2.d: src/main.rs
-
-src/main.rs:
--- 1.getting-started/2-hello-cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/rome/Documents/prog/rust/rust-intro/2-hello-cargo/target/debug/hello_cargo: /home/rome/Documents/prog/rust/rust-intro/2-hello-cargo/src/main.rs
