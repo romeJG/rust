adfb7c36e9efac70c39375d053e3f68682476202
romejg <justineguillermo00@gmail.com>
2024-06-17 21:08:27 +0800

    remved target folder from git

--- 2.guessing-game/guessing-game/target/debug/deps/guessing_game-4187744c7f7d8b30.d
+++
@@ -1,5 +0,0 @@
-/home/rome/Documents/prog/rust/2.guessing-game/guessing-game/target/debug/deps/guessing_game-4187744c7f7d8b30: src/main.rs
-
-/home/rome/Documents/prog/rust/2.guessing-game/guessing-game/target/debug/deps/guessing_game-4187744c7f7d8b30.d: src/main.rs
-
-src/main.rs:
--- 2.guessing-game/guessing-game/target/debug/guessing-game.d
+++
@@ -1,1 +0,0 @@
-/home/rome/Documents/prog/rust/2.guessing-game/guessing-game/target/debug/guessing-game: /home/rome/Documents/prog/rust/2.guessing-game/guessing-game/src/main.rs
